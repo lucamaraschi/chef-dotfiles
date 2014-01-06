d1d91b84a109b0480334302a57a3b53660913fa4
Luca Maraschi <luca@icemobile.com>
2014-01-06 15:09:17 +0100

    added zsh

---
+++ recipes/zsh.rb
@@ -0,0 +1,7 @@
+include_recipe "applications::git"
+
+execute "oh my zsh install" do
+  command "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
+  user node['current_user']
+  not_if { File.exist?("#{node['home']}/.oh-my-zsh") }
+end
