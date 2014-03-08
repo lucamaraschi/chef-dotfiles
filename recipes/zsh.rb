94940a548313e8a301aa1e08f527974c3f4812f6
Luca <luca.maraschi@gmail.com>
2014-03-08 05:48:38 +0100

    Removed git reference in zsh recipe

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -1,5 +1,3 @@
-include_recipe "applications::git"
-
 execute "oh my zsh install" do
   command "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
   user node['current_user']
